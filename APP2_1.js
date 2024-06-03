5b654a01858f1eb5d034f1fcd6eecac822c643e5
blas116409 <156983935+blas116409@users.noreply.github.com>
2024-06-03 16:56:51 -0600

    Add files via upload

---
+++ APP2_1.js
@@ -0,0 +1,20 @@
+let titulo = document.querySelector ("h1") ;
+titulo.innerHTML = " HORA DEL DESADIO " ;
+
+function intento2 (){
+   console.log("EL BOTON FUE CLICADO ") ;
+}
+
+function intento3 (){
+   let ciudad = prompt ("MENCIONA UNA CIUDAD DE BRASIL ") ;
+ alert ("ESTUVE EN " + ciudad + " ME ACORDE DE TI ") ; 
+}
+
+function intento4 () {
+    alert ("YO AMO JS ") ;
+}
+
+function intento5 () {
+    let num1 = prompt ("MENCIONA UN NUMERO ") ;
+    let num2 = prompt ("MENCIONA EL SEGUNDO NUMERO ") ;
+    alert (num1 + num2 ) ;}
